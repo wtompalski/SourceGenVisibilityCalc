af573f917f770ab1cb035200f79d22ec88c7de69
Wojciech Tompalski <wojciech.tompalski@capgemini.com>
2021-12-14 21:12:47 +0100

    Add runner

---
+++ Run/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+
+
+Expressions.ExpressionParser parser = new();
+
+var expCompany = parser.Parse("IsCompany");
+var expPerson = parser.Parse("!IsCompany");
+
+
+var code = new ExpressionCompiler.ExpressionCompiler().CompileExpressions(new Dictionary<string, Expressions.Expression>
+{
+    { "CompanyDetailsVisible", expCompany },
+    { "PersonDetailsVisible", expPerson }
+});
+
+Console.WriteLine(code);
